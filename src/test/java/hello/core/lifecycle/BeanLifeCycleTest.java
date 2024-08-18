b23e313fd56aa0faca94edabd1230ce6fd1a8131
yechan <rladPcks980@gmail.com>
2024-08-18 20:16:41 +0900

    Provider이용해서 웹 스코프

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -1,13 +1,11 @@
 package hello.core.lifecycle;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class BeanLifeCycleTest {
 
@@ -18,8 +16,6 @@ class BeanLifeCycleTest {
         ac.close(); // 스프링 컨테이너를 종료, ConfigurableApplicationContext 필요
     }
 
-
-
     @Configuration
     static class LifeCycleConfig{
         @Bean
@@ -27,7 +23,6 @@ class BeanLifeCycleTest {
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("http://hello-spring.dev");
             return networkClient;
-
         }
     }
 }
---
+++ src/test/java/hello/core/scope/PrototypeProviderTest.java
@@ -0,0 +1,71 @@
+package hello.core.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import jakarta.inject.Provider;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+public class PrototypeProviderTest {
+
+    @Test
+    void providerTest() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+        Assertions.assertThat(count1).isEqualTo(1);
+
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean2.logic();
+        Assertions.assertThat(count2).isEqualTo(1);
+    }
+
+
+    static class ClientBean {
+        //        @Autowired
+//        private ApplicationContext ac;
+//        @Autowired // 스프링 컨테이너에서 대신 조회해주는 대리자 정도로 생각하면 된다. 단 스프링에 의존적이다.
+        // getObject() 로 호출가능
+//        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+
+        @Autowired // 스프링에 의존적이지 않은 방법 .get() 으로 호출가능
+        private Provider<PrototypeBean> prototypeBeanProvider;
+
+        public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
+            prototypeBean.addCount();
+            int count = prototypeBean.getCount();
+            return count;
+        }
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+
+        private int count = 0;
+
+        public void addCount() {
+            count++;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init" + this);
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+}
